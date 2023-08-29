deb9429f693ce94bdf8999c72475db2584a1e2b4
Ryan <ryanmortuaire@gmail.com>
2023-08-29 17:01:46 +0200

    creation du systeme de routing

---
+++ kasa-app/src/components/header/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+      <header className="header">
+        <figure className="header__fig">
+          <img className="logo" src="" alt="logo de l'agence kasa" />
+        </figure>
+        {/* <Nav className="nav-header" /> */}
+      </header>
+    );
+  }
