b3ed84966b6824afebc66f01566986e796ead62f
Ryan <ryanmortuaire@gmail.com>
2023-10-04 07:33:39 +0200

    correction

--- kasa-app/src/components/carousel/Carousel.js
+++ kasa-app/src/components/carousel/Carousel.js
@@ -1,4 +1,4 @@
-import classes from './carousel.module.scss';
+import './carousel.scss';
 import rightChevron from '../../assets/chevron_carousel_right.png';
 import leftChevron from '../../assets/chevron_carousel_left.png';
 import { useState } from 'react'
@@ -20,7 +20,7 @@ export default function Slider({imageSlider}) {
     }
 
     return (
-        <section style={{backgroundImage : `url(${imageSlider[currentIndex]})`}} className={classes.carousel}>
+        <section style={{backgroundImage : `url(${imageSlider[currentIndex]})`}} className='carousel'>
             {imageSlider.length > 1 && 
                 <>
                     <img 
