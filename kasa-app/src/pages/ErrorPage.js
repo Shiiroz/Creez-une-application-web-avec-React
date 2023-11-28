343c16ee6c78d74df53dd4638ce648719230b913
Ryan <ryanmortuaire@gmail.com>
2023-11-28 19:08:36 +0100

    scss

--- kasa-app/src/pages/ErrorPage.js
+++ kasa-app/src/pages/ErrorPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/header/header';
 import Error from '../components/Error/Error';
-import classes from './Error.muodules.scss';
 
 function ErrorPage() {
   return (
